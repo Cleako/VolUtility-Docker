8cb2d635a2859d4f41d785b1657cebc671a7fcab
Marwolf <cleako@gmail.com>
2018-07-28 00:09:03 -0400

    Initial commit

---
+++ Start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ""
+sudo make restart
+echo ""
+echo ""
+echo "Ready to access. Visit http://localhost:8080 to begin."
+echo ""
+echo "When adding your image, it will ask to specify the full path of your memory dump. Copy your image into the memory-dump folder and then specify:"
+echo ""
+echo "/opt/images/<your_memory_dump_filename>"
+echo ""
---
+++ Stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo ""
+sudo make stop
+echo ""
